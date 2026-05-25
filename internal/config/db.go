0caf58d9dfc73e71941adbc599e057efc41848b5
gongskie791 <markjosephtiempo1@gmail.com>
2026-05-25 19:23:37 +0800

    incorrect database credentials

--- internal/config/db.go
+++ internal/config/db.go
@@ -14,7 +14,13 @@ type DB struct {
 }
 
 func NewDB() *DB {
-	dsn := os.Getenv("DATABASE_URL")
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=5432 sslmode=require",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
 	if dsn == "" {
 		panic("DATABASE_URL environment variable is not set")
 	}
